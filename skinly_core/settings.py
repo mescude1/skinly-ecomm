b51641b8c948e2d3d2d67df7cbbeaff909163739
Juan Esteban Martinez <juanestebanmartinezbotero@gmail.com>
2025-11-04 15:21:59 -0500

    carpeta locale

--- skinly_core/settings.py
+++ skinly_core/settings.py
@@ -46,6 +46,7 @@ AUTH_USER_MODEL = "skinly.User"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -106,13 +107,25 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+from django.utils.translation import gettext_lazy as _
+
+LANGUAGE_CODE = "es"  # idioma por defecto
+
+LANGUAGES = [
+    ('es', _('Español')),
+    ('en', _('English')),
+]
 
 TIME_ZONE = "UTC"
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
+
 
 
 # Static files (CSS, JavaScript, Images)
